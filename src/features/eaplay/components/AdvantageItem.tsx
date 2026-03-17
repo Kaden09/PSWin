dd4affb505881693a689f0292962cba147afde2c
Maxim Volkov <ketkette7@gmail.com>
2026-03-17 14:14:51 +0400

    feat: created advantage items

---
+++ src/features/eaplay/components/AdvantageItem.tsx
@@ -0,0 +1,22 @@
+interface AdvantageItemProps {
+    icon: React.ReactNode;
+    title: string;
+    className?: string;
+}
+
+function AdvantageItem({icon, title, className = ""}: AdvantageItemProps) {
+    return (
+      <div className="flex flex-col gap-3 bg-secondary-background border border-primary-border rounded-2xl py-4 px-6">
+        <div className="flex gap-3 items-center">
+          <div className="bg-tertiary-background rounded-lg p-2">{icon}</div>
+          <h3 className="">{title}</h3>
+        </div>
+        <p className="text-secondary-font">
+          В коллекцию The Play List входят более 50 игр из ваших любимых франшиз вроде Need for
+          Speed, Madden и Battlefield.
+        </p>
+      </div>
+    );
+}
+
+export default AdvantageItem;
--- src/features/main/components/Main.tsx
+++ src/features/main/components/Main.tsx
@@ -1,10 +1,13 @@
 import { FeedbackItem } from "@/features/feedback";
 import { HeroSection } from "@/features/hero";
+import { AdvantageItem } from "@/features/eaplay";
+import { Gamepad } from "lucide-react";
 
 function Main() {
     return (
         <main>
             <HeroSection />
+            <AdvantageItem icon={<Gamepad />} title="Title"/>
             <FeedbackItem />
         </main>
     )
